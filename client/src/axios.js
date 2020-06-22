039f8069d0f3f14d361e7589b2d2748033fe127a
Brennan Wilkins <bwilkins767@gmail.com>
2020-06-22 17:42:58 -0500

    fixed production axios path

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:9000/api/'
+  baseURL: 'https://guitar-trainer.herokuapp.com/api/'
 });
 
 export const authInstance = axios.create({
-  baseURL: 'http://localhost:9000/api/auth/'
+  baseURL: 'https://guitar-trainer.herokuapp.com/api/auth'
 });
